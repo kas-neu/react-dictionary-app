448ca34f4bf566fe66b9089189a4dd65abfee0e1
kas-neu <153776294+kas-neu@users.noreply.github.com>
2024-05-05 13:11:36 +0200

    included api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 const Dictionary = () => {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
   }
+  const apiKey = "t0f9aeaa40c036e39cfbab4e7a210ao8";
+  let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+  axios.get(apiUrl).then(handleResponse);
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
