df527cdabbb1370ed51956f334bdd63ddacb272e
kas-neu <153776294+kas-neu@users.noreply.github.com>
2024-05-05 12:20:35 +0200

    created footer section with links

--- src/App.js
+++ src/App.js
@@ -4,10 +4,33 @@ import logo from "./logo.png";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-        <h1>Dictionary</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <h1>Dictionary</h1>
+        </header>
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://github.com/kas-neu"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Kasia
+            </a>{" "}
+            and is open-sourced on{" "}
+            <a
+              href="https://github.com/kas-neu/react-dictionary-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            .
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
